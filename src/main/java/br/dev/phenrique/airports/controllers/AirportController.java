e1dcf5f0faf848667e103288bbb1b37ff7885d14
phenrique2407 <brito.phenrique@gmail.com>
2025-03-07 09:38:40 -0300

    “

--- src/main/java/br/dev/phenrique/airports/controllers/AirportController.java
+++ src/main/java/br/dev/phenrique/airports/controllers/AirportController.java
@@ -8,7 +8,9 @@ import br.dev.phenrique.airports.entities.Airport;
 import br.dev.phenrique.airports.service.AirportService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -29,5 +31,26 @@ public class AirportController {
     public List<Airport> findAll(){
         List<Airport> result = airportService.findAll();
         return result;
+    }
+    /**
+    * Endpoint /airports/city/{cityName
+    * preparando para devolver código de status conforme
+    * padronização REST.
+    * @param cityName
+    * @return
+    */
+    @GetMapping("/city/{cityName}")
+    public ResponseEntity<List<Airport>> findByCityIgnoreCase(@PathVariable String cityName) {
+        List<Airport> result = airportService.findByCity(cityName);
+
+        if (result.isEmpty()) {
+            // Ops... lista vazia...
+            // notFound devolve 404
+            return ResponseEntity.notFound().build();
+        } else {
+            // Eba! Tem dados!
+            // ok devolve 200
+            return ResponseEntity.ok(result);
+        }
     }
 }
--- src/main/java/br/dev/phenrique/airports/repositories/AirportRepository.java
+++ src/main/java/br/dev/phenrique/airports/repositories/AirportRepository.java
@@ -5,6 +5,7 @@
 package br.dev.phenrique.airports.repositories;
 
 import br.dev.phenrique.airports.entities.Airport;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
@@ -12,5 +13,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * @author sesidevb
  */
 public interface AirportRepository extends JpaRepository<Airport, Long> {
-    
+   List<Airport> findByCityIgnoreCase(String city);
+   
 }
